194edcda60df6656e058b6fbe9efcce3e149e6e1
sujithrajendran <67055680+sujithrajendran@users.noreply.github.com>
2025-07-03 12:02:05 +0530

    Create Task.ts

---
+++ src/model/Task.ts
@@ -0,0 +1,9 @@
+export class Task {
+  static readonly taskName: string;
+  static readonly taskId: number;
+  static readonly description: string;
+  static readonly status: string;
+  static readonly createdAt: string;
+  static readonly createdBy: string;
+  static readonly priority: string;
+}
