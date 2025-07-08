8b88217dd0e4dbda0bd14245dccc853569327d13
sujithrajendran <67055680+sujithrajendran@users.noreply.github.com>
2025-07-08 16:46:28 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,21 @@ import express from "express";
 import taskRoutes from "./router/Router";
 import cors from "cors";
 import { LoggerFactory } from "./Logger/LoggerFactory";
+const rateLimit = require("express-rate-limit");
 
 const logger = LoggerFactory.getLogger();
 const app = express();
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 200,
+  keyGenerator: (req: any) => req.ip,
+  skip: (req: any) => req.user && req.user.isAdmin,
+  handler: (req: any, res: any) => {
+    res.status(429).json({ error: "Calm down! Try again later." });
+  }
+});
+app.use(limiter);
+
 app.use(
   cors({
     origin: "https://taskmanagement-4l0e.onrender.com",
