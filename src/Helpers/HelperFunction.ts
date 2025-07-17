18cef3ecfe0a2b7084542c8473c3684e7f6639c1
sujithrajendran <67055680+sujithrajendran@users.noreply.github.com>
2025-07-17 12:53:03 +0530

    Update HelperFunction.ts

--- src/Helpers/HelperFunction.ts
+++ src/Helpers/HelperFunction.ts
@@ -1,7 +1,12 @@
 import { Db } from "mongodb";
+import { DBConnectionService } from "../dbService/DbConnectionService";
+import { User } from "../model/User";
+import { LoggerFactory } from "../Logger/LoggerFactory";
 
 const SIGNIN_COLLECTION = "signin";
 const Task_COLLECTION = "task";
+const DATABASE = "kea_application_info";
+const logger = LoggerFactory.getLogger();
 
 export class HelperFunction {
   async getNextTaskIdFromDB(db: Db): Promise<number> {
@@ -28,5 +33,27 @@ export class HelperFunction {
     const lastId = parseInt(lastTask[0]?.userId || "100", 10);
     const nextId = lastId + 1;
     return nextId;
+  }
+
+  async insertUser(email: string, username?: string) {
+    (async () => {
+      try {
+        if (!username) {
+          username = email.split("@")[0];
+        }
+        const db = await new DBConnectionService().getDBConnection(DATABASE);
+        const existingUser = await db
+          .collection(SIGNIN_COLLECTION)
+          .findOne({ email, isActive: true });
+        if (!existingUser) {
+          logger.info(`Inside inserting user details ::`);
+          const userId = await new HelperFunction().getNextUserIdFromDB(db);
+          const user = new User(username, "", email, new Date(), userId, true);
+          await db.collection(SIGNIN_COLLECTION).insertOne(user);
+        }
+      } catch (err) {
+        logger.error("Background user insert failed", err);
+      }
+    })();
   }
 }
