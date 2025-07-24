8f00f9389c5111d766a5939a09a84b6dc674b99b
sujithrajendran <67055680+sujithrajendran@users.noreply.github.com>
2025-07-24 15:54:41 +0530

    Update LoggerFactory.ts

--- src/Logger/LoggerFactory.ts
+++ src/Logger/LoggerFactory.ts
@@ -1,5 +1,8 @@
 // logger.ts
 import { createLogger, format, transports, Logger } from "winston";
+import { getNamespace } from "cls-hooked";
+import { v4 as uuidv4 } from "uuid";
+
 const { combine, timestamp, printf, colorize } = format;
 
 export class LoggerFactory {
@@ -7,8 +10,8 @@ export class LoggerFactory {
 
   public static getLogger(): Logger {
     if (!LoggerFactory.logger) {
-      const customFormat = printf(({ level, message, timestamp }) => {
-        return `[${timestamp}] ${level}: ${message}`;
+      const customFormat = printf(({ level, message, timestamp, traceId }) => {
+        return `[${timestamp}] ${level}: [${traceId}] ${message}`;
       });
 
       LoggerFactory.logger = createLogger({
@@ -16,15 +19,17 @@ export class LoggerFactory {
         format: combine(
           colorize(),
           timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+          format((info) => {
+            const ns = getNamespace("traceId");
+            info.traceId = ns?.get("correlationId") || uuidv4();
+            return info;
+          })(),
           customFormat
         ),
-        transports: [
-          new transports.Console(),
-        //   new transports.File({ filename: "logs/error.log", level: "error" }),
-        //   new transports.File({ filename: "logs/combined.log" })
-        ]
+        transports: [new transports.Console()]
       });
     }
+
     return LoggerFactory.logger;
   }
 }
