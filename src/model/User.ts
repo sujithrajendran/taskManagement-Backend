4894e6915c72032ecf91b291374a329be1c33411
sujithrajendran <67055680+sujithrajendran@users.noreply.github.com>
2025-07-25 13:30:08 +0530

    Update User.ts

--- src/model/User.ts
+++ src/model/User.ts
@@ -5,18 +5,22 @@ export class User {
   createdAt: Date;
   userId: number;
   isActive: boolean;
+  isSocialSignin: boolean;
   constructor(
     userName: string,
     password: string,
     email: string,
     createdAt: Date,
     userId: number,
+    isActive: boolean,
+    isSocialSignin: boolean
   ) {
     this.userName = userName;
     this.password = password;
     this.email = email;
     this.createdAt = createdAt;
     this.userId = userId;
-    this.isActive = true;
+    this.isActive = isActive;
+    this.isSocialSignin = isSocialSignin;
   }
 }
