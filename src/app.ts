e8b07644ca5f0f99e5c54507aca18ad3f7281a07
sujithrajendran <67055680+sujithrajendran@users.noreply.github.com>
2025-08-22 12:06:14 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,11 +17,11 @@ const io = new Server(server, {
     origin: "https://taskmanagement-4l0e.onrender.com"
   }
 });
-app.use(
-  cors({
-    origin: "https://taskmanagement-4l0e.onrender.com",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://taskmanagement-4l0e.onrender.com",
+//   })
+// );
 
 app.use(server, {
   cors: {
